4356a4d8bdf97ebeb8eb4cfe34a51faeed79e704
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-02-18 08:46:06 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,47 @@
 class BooksController < ApplicationController
-  
-  
+
+ def new
+
+ end
+
+  def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+      @user = current_user
+    	@book = Book.find(params[:id])
+    end
+
+ def create
+    @book = Book.new(book_params)
+		@book.user_id = current_user.id
+	  @book.save
+    redirect_to  book_path(@book.id)
+ end
+
+ def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+ end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+     @book = Book.find(params[:id])
+     @book.destroy
+     redirect_to "/books"
+   end
+
+
+ private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = current_user
+    @users = User.all
+    @book =Book.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+   @user = User.find(params[:id])
+  end
+
+   def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(@user.id)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
